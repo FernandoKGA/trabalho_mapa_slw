c23fb33769c6ec0c8cf5fb1dfdb1a8260698ce3d
FernandoKGA <fernando.kga@gmail.com>
2018-09-23 21:56:42 -0300

    Adicionada geolocalização

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, height=device-height, user-scalable=yes">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.js'></script>
+    <script src='modernizr-custom.js'></script>
     <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' rel='stylesheet' />
 </head>
 <body>
@@ -29,19 +30,61 @@
     </div>
     <div id='map' style="width: auto; height: 400px;"></div>
     <script>
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG8ta2dhIiwiYSI6ImNqbTlpcnMxbDAwMGMzcG9nNmtldWlmYWQifQ.O4LmFPbru9U4X10QHhv1kQ';
-        var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/fernando-kga/cjmecq5bqea3l2rp07iyhzo90',
-        center: [-46.631,-23.554],
-        zoom: 13.0
-        });
-        map.addControl(new mapboxgl.GeolocateControl({
-        positionOptions: {
-        enableHighAccuracy: true
-        },
-        trackUserLocation: true
-        }));
+
+        //Pega a posicao da pessoa
+        function get_location(){
+            if(Modernizr.geolocation){
+                navigator.geolocation.getCurrentPosition(sucesso,erro,opcoes);
+            }
+            else{
+                alert("Seu navegador não suporta Geolocalização.");
+            }
+        }
+        var opcoes = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+        get_location();
+
+        function erro(error){
+            switch(error.code){
+                case error.PERMISSION_DENIED:
+                    alert("Você recusou a Geolocalização, não posso abrir o mapa para você...");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert("Informação da localização indisponível!");
+                    break;
+                case error.TIMEOUT:
+                    alert("O pedido ao usuário passou do tempo.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    alert("Um erro estranho aconteceu... Infelizmente não sei o que é...");
+                    break;
+            }
+        }
+
+        function sucesso(pos){
+            mapboxgl.accessToken = 'pk.eyJ1IjoiZmVybmFuZG8ta2dhIiwiYSI6ImNqbTlpcnMxbDAwMGMzcG9nNmtldWlmYWQifQ.O4LmFPbru9U4X10QHhv1kQ';
+        
+            var geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+            enableHighAccuracy: true
+            },
+            trackUserLocation: true
+            });
+
+            var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/fernando-kga/cjmecq5bqea3l2rp07iyhzo90',
+            center: [-46.631,-23.554],
+            zoom: 13.0,
+            });
+            map.addControl(geolocate);
+            map.scrollZoom.enable({around: 'center'});
+            map.trigger();
+        }
+        
     </script>
 </body>
 </html>
